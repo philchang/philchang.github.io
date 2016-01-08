29fd2575592a7bf214df2ff4ae37dd9e4c354cc0
Philip Chang <chang65@uwm.edu>
2016-01-08 14:21:41 -0600

    fixed up newtons laws and menus include

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+document.write('\
+                                <h1><a href="index.html">Physics Helper</a></h1>\
+				<nav id="nav">\
+					<ul>\
+						<li><a href="index.html">Home</a></li>\
+						<li><a href="mechanics/index.html">Mechanics</a></li>\
+						<li><a href="em/index.html">E&M</a></li>\
+						<li><a href="#about_us">About Us</a></li>\
+						<li><a href="talkback.html">Feedback</a></li>\
+					</ul>\
+				</nav>\
+');
